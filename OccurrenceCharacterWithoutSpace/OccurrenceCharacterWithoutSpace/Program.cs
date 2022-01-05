a5fd506439b0ebefa6c231203c04224efc21a1fd
Rohul Amin Mohammad Dipu <41869272+radipu@users.noreply.github.com>
2022-01-06 01:44:05 +0600

    first commit

---
+++ OccurrenceCharacterWithoutSpace/OccurrenceCharacterWithoutSpace/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace OccurrenceCharacterWithoutSpace
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            string str =  Console.ReadLine();
+            Console.WriteLine("\nInput: " + str);
+
+            str = str.Replace(" ", string.Empty);
+
+            while (str.Length > 0)
+            {
+                Console.Write(str[0] + " = ");
+                int count = 0;
+
+                for (int j = 0; j < str.Length; j++)
+                {
+                    if (str [0] == str [j])
+                        count++;
+                }
+                Console.WriteLine(count);
+                str = str.Replace(str[0].ToString(), string.Empty);
+            }
+            Console.ReadLine();
+        }
+    }
+}
